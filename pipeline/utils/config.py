54c07ef6c2679b93ec3a4a320903f20ce7cfc137
Ivan Solomichev <ivan.solomichev@gmail.com>
2026-04-15 17:37:54 -0500

    feat: add snowflake connector

--- pipeline/utils/config.py
+++ pipeline/utils/config.py
@@ -19,3 +19,11 @@ AWS_ACCESS_KEY_ID: str = _require_env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY: str = _require_env("AWS_SECRET_ACCESS_KEY")
 AWS_REGION: str = _require_env("AWS_REGION")
 S3_BUCKET_RAW: str = _require_env("S3_BUCKET_RAW")
+
+# Snowflake
+SNOWFLAKE_ACCOUNT: str = _require_env("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_USER: str = _require_env("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD: str = _require_env("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_DATABASE: str = _require_env("SNOWFLAKE_DATABASE")
+SNOWFLAKE_SCHEMA: str = _require_env("SNOWFLAKE_SCHEMA")
+SNOWFLAKE_WAREHOUSE: str = _require_env("SNOWFLAKE_WAREHOUSE")
